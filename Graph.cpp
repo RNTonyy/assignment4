9e95085ab960cbd470f55837779c41d0dd444030
NathanChase22 <ntc477@utexas.edu>
2024-10-26 16:22:17 -0500

    implementing adding

--- Graph.cpp
+++ Graph.cpp
@@ -1,29 +1,39 @@
 #include "Graph.h"
 #include <boost/graph/graph_traits.hpp>
 
-tony::Graph::Graph(int num_nodes, int num_edges) : 
+using namespace tony;
+
+Graph::Graph(int num_nodes, int num_edges) : 
     num_edges(num_edges), num_nodes(num_nodes), csr(BoostCSR(num_nodes)) {};
 
-tony::Graph::~Graph(){};
+Graph::~Graph(){};
 
-void tony::Graph::add_edge(int src, int dest, int val) {
+void Graph::add_edge(int src, int dest, int new_val) {
     //check if there already exists a parallel edge
-    Vertex u = boost::vertex(src, csr);
-    Vertex v = boost::vertex(dest, csr);
+    Vertex u = boost::vertex(src, lst);
+    Vertex v = boost::vertex(dest, lst);
 
-    boost::add_edges(u,v,csr);
+    Edge e;
+    // returns both the actual edge and the state of its existence
+    // std::pair is roughly similar to tulip in that it allows you to couple 
+    // two unmutable pieces of data together in a pair
+    std::pair<Edge, bool> edge_pair = boost::edge(u,v,lst);
 
-    // if parallel image exists, put either the same value or the newly updated value depending on which one is larger
+    //check if edge exists
+    if (edge_pair.second) {
+        e = edge_pair.first;
+        int cur_val = boost::get(boost::edge_weight, lst, e);
+        if (new_val > cur_val) {
+            boost::put(boost::edge_weight, lst, e, new_val);
+        }
+    } else { // edge currently DNE 
+        boost::add_edge(u,v,new_val,lst);
+    }
+}
 
-
-
+void Graph::finalize() {
+    // Convert the adjacency list to a CSR graph
     
     
 
-
-
-
-    //if parallel edge, either remove 
-
-    // O.W keep
 }
--- Graph.h
+++ Graph.h
@@ -1,10 +1,15 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <boost/graph/compressed_sparse_row_graph.hpp>
 #include <boost/property_map/property_map.hpp>
-#include <boost/graph/graph_traits.hpp>
 
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/properties.hpp>
 
 //.h define the class we want
 
@@ -33,21 +38,45 @@
     
   */
 namespace tony {
-    struct NodeLabel {
-        int label;
-    };
+    /**
+     * NOTE: because PageRank will be modifying a vector outside of our graph 
+     *  by the time we are computing it, we will NOT need to modify the CSR.
+     */
+    
+    // struct NodeLabel {
+    //     int label;
+    // };
+
+    /**
+     * NOTE: BoostCSR is Read-Only
+     */
+    
+    //variable BoostCSR will be an alias for this long complicated template
+    using BoostCSR = boost::compressed_sparse_row_graph<
+        boost::directedS, // graph will be directed 
+        boost::no_property, //
+        boost::property<boost::edge_weight_t, int>>;
+    
+    using AdjacencyList = boost::adjacency_list<
+        boost::vecS, //vertices will be stored in a vector
+        boost::vecS, //edges will be stored in a vector
+        boost::directedS, // directed edges
+        boost::no_property, // vertex labels, which will be none in our case
+        boost::property<boost::edge_weight_t,int>>; // edge weights
+    
+    using Vertex = boost::graph_traits<AdjacencyList>::vertex_descriptor;
+    using Edge = boost::graph_traits<AdjacencyList>::edge_descriptor;
+
 
 
     class Graph {
-        //variable BoostCSR will be an alias for this long complicated
-        using BoostCSR = boost::compressed_sparse_row_graph<boost::directedS, NodeLabel, boost::property<boost::edge_weight_t, int>>;
-        using Vertex = boost::graph_traits<BoostCSR>::vertex_descriptor;
-
+        
     private:
         //fields
         int num_edges;
         int num_nodes;
         BoostCSR csr;
+        AdjacencyList lst;
     
     
     public:
@@ -76,13 +105,26 @@ namespace tony {
          */
         void add_edge(int src, int dest, int val);
 
+
+        /**
+         * QUESTION: Do we NEED this update_label? Because we want to only populate 
+         *  the adjacency list 
+         */
+
         /**
          * INPUT: src node (row ptr), and a new label
          * OUTPUT: label gets updated, old label is returned
          */
-        int update_label(int src, int label);
+        // int update_label(int src, int label);
         
+        /**
+         * INPUT: user gives nothing, should use the graph object's adjacency list
+         * OUTPUT: returns nothing, creates a read-only CSR
+         */
+        
+        void finalize();
+
     }; 
  };
 
-
+#endif
